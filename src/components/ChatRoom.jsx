510fd624f0f5fb7e753ab0edd1fbaaa4743fc861
reynoldadade <reynoldadade@gmail.com>
2022-08-23 14:09:42 +0000

    setup message handler

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+
 const ChatRoom = () => {
+  // create message variable
+  const [message, setMessage] = useState("");
+  // handle when send message button is clicked
+  const sendMessageHandler = () => {
+    console.log(message);
+  };
+  //handle when input is used
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
   return (
     <div className="w-screen h-screen">
-      <div className="w-full p-2 shadow-lg bg-purple-400 text-white">
-        Chat Room
+      <div className="w-full p-2 shadow-lg bg-purple-400 text-white flex flex-col justify-between">
+        Chatroom
+      </div>
+      <div id="message-view" className="flex-grow"></div>
+      <div id="inputs" className="flex">
+        <textarea
+          type="text"
+          name="message"
+          className="border rounded-lg flex-grow"
+          value={message}
+          onChange={handleChange}
+        />
+        <button onClick={sendMessageHandler}>Send</button>
       </div>
     </div>
   );

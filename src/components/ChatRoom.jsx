103a96962aeba7309e074502a0469dfe971c8e36
reynoldadade <reynoldadade@gmail.com>
2022-08-25 07:45:28 +0000

    test for content to show on screen showing after message is sent

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -63,6 +63,7 @@ const ChatRoom = () => {
       >
         <div
           className="overflow-y-scroll flex-grow h-72 flex-col flex w-full"
+          data-testid="chatRoomContainer"
           onScroll={handleScroll}
         >
           {currentChats.map((chat, index) => (
@@ -77,24 +78,26 @@ const ChatRoom = () => {
             className="border rounded-lg flex-grow"
             value={message}
             onChange={handleChange}
+            data-testid="messageBox"
           />
           <button
             onClick={sendMessageHandler}
             className="bg-blue-500 text-white rounded-lg py-1 px-4 flex items-center disabled:bg-gray-500 disabled:bg-opacity-50"
             disabled={!message}
+            data-testid="sendButton"
           >
             <span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="w-4 h-4"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
                 />
               </svg>
